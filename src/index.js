7d07c8c4493a37b14638fda94844841d4fe93ff9
Corey Rowe <coreyr1196@gmail.com>
2018-07-13 00:06:47 -0400

    Set up material-ui and custom color scheme

--- src/index.js
+++ src/index.js
@@ -1,8 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('server-root'));
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            light: '#58a5f0',
+            main: '#0277bd',
+            dark: '#004c8c',
+            contrastText: '#fff',
+        },
+        secondary: {
+            light: '#82f7ff',
+            main: '#40c4ff',
+            dark: '#0094cc',
+            contrastText: '#000',
+        },
+        background: {
+            default: '#003865', // TARDIS Blue!
+        }
+    },
+});
+
+function App() {
+    return (
+        <MuiThemeProvider theme={theme}>
+            <React.Fragment>
+                <CssBaseline />
+            </React.Fragment>
+        </MuiThemeProvider>
+    );
+}
+
+ReactDOM.render(<App />, document.querySelector('#server-root'));
 registerServiceWorker();
